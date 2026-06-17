efb2d7aaa5732508fb9e7e9fb4e3e8af02cedff4
Rohith-2504 <brohithreddy9731@gmail.com>
2026-06-17 20:38:37 +0530

    Deploy: trigger fresh build with frontend header comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+// Aetheria Client App Shell - Dynamic Routing & State Controller
 import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
